a3851280193386deb9c1c6b522e07da93c8b95d1
frame88 <fleuzzi97@gmail.com>
2021-11-18 16:26:37 +0100

    printf

--- js/script.js
+++ js/script.js
@@ -9,9 +9,11 @@ Copiate questa descrizione nel vostro file js come commento.
 Datevi sempre 5 minuti di tempo per capire bene cosa venga richiesto, poi passate a scomporre in italiano i problemi ed infine traducete in JS.
 */
 
-let name = prompt('inserisci il tuo nome: ');
+let namez = prompt('inserisci il tuo nome: ');
 let surname = prompt('inserisci il tuo cognnome: ');
 let color = prompt('inserisci il tuo colore preferito: ');
+
+console.log(namez + surname + color);
 
 
 
