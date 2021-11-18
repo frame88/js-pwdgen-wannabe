607696325e542b382c45cc27533a822fec7b2724
frame88 <fleuzzi97@gmail.com>
2021-11-18 16:09:44 +0100

    let variable

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+Descrizione esercizio:
+1.  Chiedi all’utente il suo nome,
+2.  poi chiedi il suo cognome,
+3.  poi chiedi il suo colore preferito
+4.  Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+Copiate questa descrizione nel vostro file js come commento.
+Datevi sempre 5 minuti di tempo per capire bene cosa venga richiesto, poi passate a scomporre in italiano i problemi ed infine traducete in JS.
+*/
+
+let name = prompt('inserisci il tuo nome: ');
+let surname = prompt('inserisci il tuo cognnome: ');
+let color = prompt('inserisci il tuo colore preferito: ');
+
+
+
