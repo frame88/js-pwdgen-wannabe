0cbdbad80fa8638de16c27b318a3caf9a60ecb61
frame88 <fleuzzi97@gmail.com>
2021-11-18 17:39:57 +0100

    completato e risolto tutto

--- js/bonus.js
+++ js/bonus.js
@@ -22,20 +22,20 @@ console.log(passwordbonus);
 document.getElementById('passwordbonus').innerHTML = passwordbonus;
 */
 
-let namez = prompt('inserisci il tuo nome: ');
-console.log(namez);
-let surname = prompt('inserisci il tuo cognnome: ');
-console.log(surname);
-let color = prompt('inserisci il tuo colore preferito: ');
-console.log(color);
+let namess = prompt('inserisci il tuo nome: ');
+console.log(namess);
+let surnamez = prompt('inserisci il tuo cognnome: ');
+console.log(surnamez);
+let colorz = prompt('inserisci il tuo colore preferito: ');
+console.log(colorz);
 
 let numeropre = parseInt(prompt('Inserisci il tuo numero preferito'));
 console.log(numeropre);
-numeropresum = numeropre + 5;
-console.log(numeropresum);
+numeropresumz = numeropre + 5;
+console.log(numeropresumz);
 
 let passwordbonus = `
-       ${namez}${surname}${color}${numeropresum}
+       ${namess}${surnamez}${colorz}${numeropresumz}
      `;
 
 console.log(passwordbonus);
